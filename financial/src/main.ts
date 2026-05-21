44975c25252791b40e38ec6e7afb434e9dc184a9
Arthur <arthur-soares@hotmail.com>
2026-05-21 16:39:18 -0300

    feat(logging): add LoggingInterceptor for HTTP request logging

--- financial/src/main.ts
+++ financial/src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   const port = process.env.PORTA || 3004;
   await app.listen(port);
   console.log(`[${process.env.APP_NAME}] running on port ${port}`);
 }
 bootstrap();
+
